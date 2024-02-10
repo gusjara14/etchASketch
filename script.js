49d990e9b343187b64540558f29349906c824ad5
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 14:42:38 -0700

    Change for loop to add divs depending on grid size
    Remove the process of adding divs one after another. Now what will
    happen is that we will create a grid of divs depending on the gridSize
    binding. We create a new div(rowDiv) and start adding divs(newDiv) to
    that div until we reach the gridSize value. Once that value is reached,
    we create a new rowDiv and repeat until the amount of rowDivs reaches
    the gridSize value.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 console.log("testtesttest");
 
 let mainDiv = document.querySelector(".main");
+let gridSize = 16;
 
-for(let index=1;index<=256;index++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("etch");
-    newDiv.textContent = index;
-    mainDiv.appendChild(newDiv);
-    newDiv.addEventListener("mouseover", function() {
-        newDiv.style.backgroundColor = "black";
-    })
+for(let index=1;index<=gridSize;index++) {
+    let newRow = document.createElement("div");
+    for(let rowIndex = 0;rowIndex<=gridSize;rowIndex++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("etch");
+        newDiv.textContent = index;
+        newDiv.style.backgroundColor = "white";
+        newRow.appendChild(newDiv);
+        newDiv.addEventListener("mouseover", function() {
+            newDiv.style.backgroundColor = "black";
+        })
+    }
+    mainDiv.appendChild(newRow);
+
+
 }
