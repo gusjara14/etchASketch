1f90e476b717f2f9e68845d5fee0698e1fa5045e
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 16:31:14 -0700

    Fix issue where extra row was being appended
    Because I had the for loop starting at an index
    of 0 and not being exited until it was equal to
    the gridSize, an extra row was being appended.
    This was fixed by starting the index at 0 and the
    exit condition being index greater than gridSize

--- script.js
+++ script.js
@@ -9,25 +9,30 @@ button.addEventListener("click", function() {
     body.appendChild(mainDiv);
     mainDiv.classList.add("main");
 
-    // let allDiv = document.querySelectorAll(".etch");
-    // document.removeChild("allDiv");
     gridSize = parseInt(prompt("Please enter a grid size between 1-100", 100));
-    if(gridSize>100) {
-        gridSize = parseInt(prompt("Please enter a grid size LESS than 100", 100));
-    } else if(gridSize<1) {
-        gridSize = parseInt(prompt("Please enter a grid size GREATER than 0", 100));
+
+    while(gridSize>=100 || gridSize<=0) {
+        gridSize = parseInt(prompt("Please enter a grid size BETWEEN 1-100", "1-100"));
+        console.log(gridSize);
     }
-    for(let index=1;index<=gridSize;index++) {
+
+    // if(gridSize>100) {
+    //     gridSize = parseInt(prompt("Please enter a grid size LESS than 100", 100));
+    // } else if(gridSize<1) {
+    //     gridSize = parseInt(prompt("Please enter a grid size GREATER than 0", 100));
+    // }
+
+    for(let index=0;index<gridSize;index++) {
         let newRow = document.createElement("div");
         newRow.classList.add("rows");
         newRow.classList.add("etch");
     
-        for(let rowIndex = 0;rowIndex<=gridSize;rowIndex++) {
+        for(let rowIndex = 0;rowIndex<gridSize;rowIndex++) {
             let newDiv = document.createElement("div");
             newDiv.classList.add("blocks");
             newDiv.classList.add("etch");
     
-            // newDiv.textContent = index;
+            newDiv.textContent = index;
             newDiv.style.backgroundColor = "white";
             newRow.appendChild(newDiv);
             newDiv.addEventListener("mouseover", function() {
