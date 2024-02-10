96940b9c960af74a43bf2a71c8e17b5467a90466
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 13:53:08 -0700

    Add js that creates multiple divs and html title
    Created a for loop that creates a new div, adds it to a "etch" class,
    makes the textContent equal to the current index value and appends
    it to the div from the html

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("testtesttest");
+
+let mainDiv = document.querySelector(".main");
+
+for(let index=1;index<255;index++) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("etch");
+    newDiv.textContent = index;
+    mainDiv.appendChild(newDiv);
+}
