5d849a77cc2bb3310fe83fd7390a1224d02982b9
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 15:11:42 -0700

    Add CSS to fill in grid and remove div textContent
    Added CSS to fill out the entire main div. Also removed the index value
    from being inside the grid divs so that they will all be equally sized.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 console.log("testtesttest");
 
 let mainDiv = document.querySelector(".main");
-let gridSize = 16;
+let gridSize = 25;
 
 for(let index=1;index<=gridSize;index++) {
     let newRow = document.createElement("div");
+    newRow.classList.add("rows");
+
     for(let rowIndex = 0;rowIndex<=gridSize;rowIndex++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("etch");
-        newDiv.textContent = index;
+
+        // newDiv.textContent = index;
         newDiv.style.backgroundColor = "white";
         newRow.appendChild(newDiv);
         newDiv.addEventListener("mouseover", function() {
