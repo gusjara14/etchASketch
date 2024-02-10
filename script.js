6e50358492ec1f38cd8490fa2dd3b1d80a57f8d3
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 14:09:42 -0700

    Add event listener to change color on mouseover
    Added an event listener in the for loop after the div has been
    appened. This event listener will run a function that changes the
    color of the div when a "mouseover" event occurs.

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ console.log("testtesttest");
 
 let mainDiv = document.querySelector(".main");
 
-for(let index=1;index<255;index++) {
+for(let index=1;index<=256;index++) {
     let newDiv = document.createElement("div");
     newDiv.classList.add("etch");
     newDiv.textContent = index;
     mainDiv.appendChild(newDiv);
+    newDiv.addEventListener("mouseover", function() {
+        newDiv.style.backgroundColor = "black";
+    })
 }
