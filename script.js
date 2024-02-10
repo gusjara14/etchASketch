5a9ea13e38ccb775fbcd12bc707d8d653dece00e
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 15:34:54 -0700

    Add button that creates a grid based on user input
    grid is created based on user input but there currently if user clicks
    button again it will not clear the current grid but add on to the grid.
    This is the next issue that needs to be addressed.

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-console.log("testtesttest");
-
+let gridSize = 0;
 let mainDiv = document.querySelector(".main");
-let gridSize = 100;
+let button = document.querySelector(".button");
 
-for(let index=1;index<=gridSize;index++) {
-    let newRow = document.createElement("div");
-    newRow.classList.add("rows");
-
-    for(let rowIndex = 0;rowIndex<=gridSize;rowIndex++) {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("etch");
-
-        // newDiv.textContent = index;
-        newDiv.style.backgroundColor = "white";
-        newRow.appendChild(newDiv);
-        newDiv.addEventListener("mouseover", function() {
-            newDiv.style.backgroundColor = "black";
-        })
+button.addEventListener("click", function() {
+    gridSize = parseInt(prompt("Please enter a grid size between 1-100", 100));
+    if(gridSize>100) {
+        gridSize = parseInt(prompt("Please enter a grid size LESS than 100", 100));
     }
-    mainDiv.appendChild(newRow);
+    for(let index=1;index<=gridSize;index++) {
+        let newRow = document.createElement("div");
+        newRow.classList.add("rows");
+    
+        for(let rowIndex = 0;rowIndex<=gridSize;rowIndex++) {
+            let newDiv = document.createElement("div");
+            newDiv.classList.add("etch");
+    
+            // newDiv.textContent = index;
+            newDiv.style.backgroundColor = "white";
+            newRow.appendChild(newDiv);
+            newDiv.addEventListener("mouseover", function() {
+                newDiv.style.backgroundColor = "black";
+            })
+        }
+        mainDiv.appendChild(newRow);
+    }
+})
 
-
-}
+gridSize = 25;
