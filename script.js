9d2186207e51b411f5b8301e1db05c3a350d68be
Gustavo Jara <78346679+gusjara14@users.noreply.github.com>
2024-02-10 16:16:06 -0700

    Remove mainDiv after button press
    Now when the button is pushed, the mainDiv will be removed from the
    body and new mainDiv will be created and appened to the body with the
    ".main" class added to it as well. Doing this will make it so that if
    there is already a grid on screen, it will remove that grid and create
    a new grid with the new dimensions that were input from the prompt.

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 let gridSize = 0;
 let mainDiv = document.querySelector(".main");
 let button = document.querySelector(".button");
+let body = document.querySelector("body");
 
 button.addEventListener("click", function() {
+    body.removeChild(mainDiv);
+    mainDiv = document.createElement("div");
+    body.appendChild(mainDiv);
+    mainDiv.classList.add("main");
+
+    // let allDiv = document.querySelectorAll(".etch");
+    // document.removeChild("allDiv");
     gridSize = parseInt(prompt("Please enter a grid size between 1-100", 100));
     if(gridSize>100) {
         gridSize = parseInt(prompt("Please enter a grid size LESS than 100", 100));
+    } else if(gridSize<1) {
+        gridSize = parseInt(prompt("Please enter a grid size GREATER than 0", 100));
     }
     for(let index=1;index<=gridSize;index++) {
         let newRow = document.createElement("div");
         newRow.classList.add("rows");
+        newRow.classList.add("etch");
     
         for(let rowIndex = 0;rowIndex<=gridSize;rowIndex++) {
             let newDiv = document.createElement("div");
+            newDiv.classList.add("blocks");
             newDiv.classList.add("etch");
     
             // newDiv.textContent = index;
